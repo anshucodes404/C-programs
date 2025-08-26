a4f0a5dad737988280d12d9e89a8056cbfba5532
Anshu Kumar <anshukr95086@gmail.com>
2025-08-26 17:19:02 +0530

    LL created

--- 19.08.25/pr1.c
+++ 19.08.25/pr1.c
@@ -7,12 +7,17 @@ struct Node
     struct Node *next;
 };
 
-void displayLL(struct Node** head){
-    if(*head == NULL){
+void displayLL(struct Node **head)
+{
+    if (*head == NULL)
+    {
         printf("Linked List is empty\n");
-    } else {
+    }
+    else
+    {
         struct Node *temp = *head;
-        while(temp != NULL){
+        while (temp != NULL)
+        {
             printf("%d --> ", temp->val);
             temp = temp->next;
         }
@@ -50,55 +55,146 @@ struct Node *createNode()
     return newNode;
 }
 
-void insertFirst(struct Node **head, int *size, struct Node** tail)
+void insertFirst(struct Node **head, int *size, struct Node **tail)
 {
-    
+
     struct Node *newNode = createNode();
 
-    if(*head == NULL){ //means this is the first element we are inserting
+    if (*head == NULL)
+    { // means this is the first element we are inserting
         *head = newNode;
         *tail = newNode;
         (*size)++;
-    } else {
+    }
+    else
+    {
         newNode->next = *head;
         *head = newNode;
         (*size)++;
     }
 }
 
-void inserEnd(struct Node** tail, int* size, struct Node** head){
+void inserEnd(struct Node **tail, int *size, struct Node **head)
+{
     struct Node *newNode = createNode();
 
-    if(*tail == NULL){
+    if (*tail == NULL)
+    {
         *head = newNode;
         *tail = newNode;
         (*size)++;
-    } else {
+    }
+    else
+    {
         (*tail)->next = newNode;
         *tail = newNode;
         (*size)++;
     }
 }
 
-void insertAtPosition(struct Node** head, int* size, struct Node** tail){
+void insertAtPosition(struct Node **head, int *size, struct Node **tail)
+{
     int pos;
     printf("Enter position: ");
     scanf("%d", &pos);
 
-    if(pos < 1 || pos > *size){
+    if (pos < 1 || pos > *size)
+    {
         printf("Position is more than the valid size(%d)\n", *size);
         return;
     }
-    if(pos == 1){
+    if (pos == 1)
+    {
         insertFirst(head, size, tail);
-    } else if(pos == *size){
+        return;
+    }
+    if (pos == *size)
+    {
         inserEnd(tail, size, head);
-    } else {
-        
+        return;
+    }
+    else
+    {
     }
 }
 
-    int main()
+void deleteFirst(struct Node **head, int *size)
+{
+    struct Node *temp = *head;
+    *head = (*head)->next;
+    free(temp);
+    (*size)--;
+}
+
+void deleteEnd(struct Node **head, struct Node **tail, int *size)
+{
+    struct Node *node = *tail;
+    struct Node *temp = *head;
+    for (int i = 1; i < (*size) - 1; i++){
+        temp = temp->next;
+    }
+    *tail = temp;
+    temp->next = NULL;
+    free(node);
+    (*size)--;
+}
+
+void deletePosition(struct Node** head, struct Node** tail, int* size){
+    struct Node *temp = *head;
+    int position;
+    printf("Enter the position want to be deleted: ");
+    scanf("%d", &position);
+    if (position < 1 || position > *size)
+    {
+        printf("Position is more than the valid size(%d)\n", *size);
+        return;
+    }
+    if(position == 1){
+        deleteFirst(head, size);
+        return;
+    } else if(position == *size){
+        deleteEnd(head, tail, size);
+        return;
+    } else {
+        int count = 1;
+        while(count < position - 1){
+            temp = temp->next;
+            count++;
+        }
+        struct Node *node = temp->next;
+        temp->next = temp->next->next;
+        free(node);
+    }
+}
+
+void deleteValue(struct Node** tail, struct Node** head, int* size){
+    int value;
+    printf("Enter value to delete: ");
+    scanf("%d", &value);
+}
+
+void totalNodes(int size){
+    printf("Total nodes in the linked List is: %d", size);
+}
+
+int searchElement(struct Node* head, struct Node* tail){
+    int element;
+    printf("Enter the element to search: ");
+    scanf("%d", &element);
+    struct Node *temp = head;
+    int index = 0;
+    printf("Searching...\n");
+    while(temp != NULL){
+        if(temp->val == element){
+            return index;
+        }
+        index++;
+        temp = temp->next;
+    }
+    return -1; //if element do not exist then this will return to the main function
+}
+
+int main()
 {
     struct Node *head = NULL;
     struct Node *tail = NULL;
@@ -142,16 +238,27 @@ void insertAtPosition(struct Node** head, int* size, struct Node** tail){
             insertAtPosition(&head, &size, &tail);
             break;
         case 6:
+            deleteFirst(&head, &size);
             break;
         case 7:
+            deleteEnd(&head, &tail, &size);
             break;
         case 8:
+            deletePosition(&head, &tail, &size);
             break;
         case 9:
+            deleteValue(&tail, &head, &size);
             break;
         case 10:
+            totalNodes(size);
             break;
         case 11:
+            int index = searchElement(head, tail);
+            if(index == -1){
+                printf("Element do not exist\n\n");
+            } else {
+                printf("Element found at index %d\n\n", index);
+            }
             break;
         case 12:
             exit(0);
